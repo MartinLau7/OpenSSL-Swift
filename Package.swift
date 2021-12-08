e999f7262caa6dd60f2ca84148ee8f27eca6e721
Martin Lau <martinlau@livemail.tw>
2021-12-08 14:43:34 +0800

    init commit

---
+++ Package.swift
@@ -0,0 +1,83 @@
+	// swift-tools-version:5.4
+	// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+extension Platform {
+#if os(macOS)
+	static let current: Platform = .macOS
+#elseif os(iOS)
+	static let current: Platform = .iOS
+#elseif os(tvOS)
+	static let current: Platform = .tvOS
+#elseif os(watchOS)
+	static let current: Platform = .watchOS
+#elseif os(Linux)
+	static let current: Platform = .linux
+#elseif os(Windows)
+	static let current: Platform = .windows
+#else
+#error("Unsupported platform.")
+#endif
+}
+
+extension Array {
+	
+	fileprivate static func collection<Element>(_ items: [[Element]]) -> [Element] {
+		return items.flatMap { $0 }
+	}
+	
+	fileprivate static func productItem<Element>(_ item: Element, when platforms: [Platform]? = nil) -> [Element] {
+		if let platforms = platforms {
+			if !platforms.contains(.current) {
+				return []
+			}
+		}
+		return [item]
+	}
+	
+	fileprivate static func productItems<Element>(_ items: [Element], when platforms: [Platform]? = nil) -> [Element] {
+		if let platforms = platforms {
+			if !platforms.contains(.current) {
+				return []
+			}
+		}
+		return items
+	}
+}
+
+let package = Package(
+	name: "OpenSSL",
+	platforms: [
+		.iOS(.v12),
+		.macOS(.v10_14),
+		.watchOS(.v7),
+		.tvOS(.v14),
+	],
+	products: [
+		// Products define the executables and libraries a package produces, and make them visible to other packages.
+		.library(
+			name: "OpenSSL",
+			targets: ["openssl"]),
+	],
+	dependencies: [
+		// Dependencies declare other packages that this package depends on.
+		// .package(url: /* package url */, from: "1.0.0"),
+	],
+	targets: .collection([
+		// Targets are the basic building blocks of a package. A target can define a module or a test suite.
+		// Targets can depend on other targets in this package, and on products in packages this package depends on.
+		.productItem(.binaryTarget(
+			name: "openssl",
+			url: "https://github.com/MartinLau7/openssl-apple/releases/download/v1.1.1l/openssl-static.xcframework.zip",
+			checksum: "db9290211ee92c3b7219f0378056fb8603c83283295d916838ae9f026f0e64d5"
+		), when: [.macOS, .iOS, .tvOS, .watchOS]),
+		.productItem(.systemLibrary(
+			name: "openssl",
+			pkgConfig: "openssl",
+			providers: [
+				.apt(["openssl libssl-dev"]),
+			]
+		), when: [.linux]),
+	])
+)
