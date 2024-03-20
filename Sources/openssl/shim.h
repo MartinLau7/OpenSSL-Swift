201d19ddf8e7ecec0839a0b4304a0069fab270ee
Martin Lau <martinlau@livemail.tw>
2024-03-20 18:21:02 +0800

    remove asn1_mac

--- Sources/openssl/shim.h
+++ Sources/openssl/shim.h
@@ -4,7 +4,6 @@
 #include <openssl/aes.h>
 #include <openssl/asn1err.h>
 #include <openssl/asn1.h>
-#include <openssl/asn1_mac.h>
 #include <openssl/asn1t.h>
 #include <openssl/asyncerr.h>
 #include <openssl/async.h>
