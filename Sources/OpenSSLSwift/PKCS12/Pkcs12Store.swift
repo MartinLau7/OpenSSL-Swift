6ccf1f57818edb08e020fcdd5b6b4373bb4a4726
𝗠𝗮𝗿𝘁𝗶𝗻 𝗟𝗮𝘂 <martinlau@livemail.tw>
2025-09-16 21:02:35 +0800

    refactor: Improve Pkcs12Store

--- Sources/OpenSSLSwift/PKCS12/Pkcs12Store.swift
+++ Sources/OpenSSLSwift/PKCS12/Pkcs12Store.swift
@@ -40,34 +40,6 @@ public final class Pkcs12Store {
         PKCS12_free(p12)
     }
 
-    public static func load(atPath path: String, password: String) throws -> Pkcs12Store {
-        guard let content = FileManager.default.contents(atPath: path) else {
-            throw CocoaError(.coderReadCorrupt)
-        }
-
-        return try content.withUnsafeBytes { buffer in
-            var bufferPtr = buffer.bindMemory(to: UInt8.self).baseAddress
-
-            guard let p12 = d2i_PKCS12(nil, &bufferPtr, numericCast(content.count)) else {
-                throw CryptoError.internalError()
-            }
-            var evpKey: OpaquePointer?
-            var x509: OpaquePointer?
-            var ca: OpaquePointer?
-            guard PKCS12_parse(p12, password, &evpKey, &x509, &ca) == 1 else {
-                var errorMessage = "Undefined error"
-                let errCode = ERR_get_error()
-                if errCode != 0 {
-                    if let errorString = ERR_error_string(errCode, nil) {
-                        errorMessage = String(cString: errorString)
-                    }
-                }
-                throw Pkcs12Error.parseError(error: errorMessage)
-            }
-            return Pkcs12Store(owning: p12, evpKey: evpKey, x509: x509, ca: ca)
-        }
-    }
-
     public func changePassword(_ oldPassword: String, to newPassword: String) -> Bool {
         return PKCS12_newpass(p12, oldPassword, newPassword) == 1
     }
@@ -78,6 +50,7 @@ public final class Pkcs12Store {
             BIO_free(out)
         }
         guard i2d_PKCS12_bio(out, p12) == 1 else {
+            var errorMessage = "Undefined error"
             let errCode = ERR_get_error()
             if errCode != 0 {
                 if let errorString = ERR_error_string(errCode, nil) {
@@ -88,7 +61,10 @@ public final class Pkcs12Store {
         }
     }
 
-//    private static func
+    public func save(toPath path: String) throws {
+        let saveURL = URL(filePath: path)
+        try save(to: saveURL)
+    }
 }
 
 public extension Pkcs12Store {
@@ -120,5 +96,48 @@ public extension Pkcs12Store {
             }
         }
         return items
+    }
+}
+
+/// Pkcs12Store loading extension
+public extension Pkcs12Store {
+    static func load(data content: Data, password: String) throws -> Pkcs12Store {
+        return try content.withUnsafeBytes { buffer in
+            var bufferPtr = buffer.bindMemory(to: UInt8.self).baseAddress
+
+            guard let p12 = d2i_PKCS12(nil, &bufferPtr, numericCast(content.count)) else {
+                throw CryptoError.internalError()
+            }
+            var evpKey: OpaquePointer?
+            var x509: OpaquePointer?
+            var ca: OpaquePointer?
+            guard PKCS12_parse(p12, password, &evpKey, &x509, &ca) == 1 else {
+                var errorMessage = "Undefined error"
+                let errCode = ERR_get_error()
+                if errCode != 0 {
+                    if let errorString = ERR_error_string(errCode, nil) {
+                        errorMessage = String(cString: errorString)
+                    }
+                }
+                throw Pkcs12Error.parseError(error: errorMessage)
+            }
+            return Pkcs12Store(owning: p12, evpKey: evpKey, x509: x509, ca: ca)
+        }
+    }
+
+    static func load(atPath path: String, password: String) throws -> Pkcs12Store {
+        guard let content = FileManager.default.contents(atPath: path) else {
+            throw CocoaError(.coderReadCorrupt)
+        }
+
+        return try load(data: content, password: password)
+    }
+
+    static func load(at url: URL, password: String) throws -> Pkcs12Store {
+        guard let content = try? Data(contentsOf: url) else {
+            throw CocoaError(.coderReadCorrupt)
+        }
+
+        return try load(data: content, password: password)
     }
 }
